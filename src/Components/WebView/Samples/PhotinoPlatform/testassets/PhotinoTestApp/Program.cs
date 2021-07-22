d3bd7c3914a87b50110a717a58e625d161651836
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-07-22 12:22:15 +0100

    Add support for configuring JS root components in Photino platform, and use it in sample

--- src/Components/WebView/Samples/PhotinoPlatform/testassets/PhotinoTestApp/Program.cs
+++ src/Components/WebView/Samples/PhotinoPlatform/testassets/PhotinoTestApp/Program.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.WebView.Photino;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -28,6 +29,7 @@ namespace PhotinoTestApp
             };
 
             mainWindow.RootComponents.Add<BasicTestApp.Index>("root");
+            mainWindow.RootComponents.RegisterForJavaScript<BasicTestApp.DynamicallyAddedRootComponent>("my-dynamic-root-component");
 
             mainWindow.Run();
         }
